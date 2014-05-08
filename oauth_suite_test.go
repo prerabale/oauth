4ee93f056f93da6b355f275e9d864f9ec2951bb2
Meaglith Ma <genedna@gmail.com>
2014-05-08 08:56:48 -0400

    Add ginkgo test support

---
+++ oauth_suite_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+
+  "testing"
+)
+
+func TestOauth(t *testing.T) {
+  RegisterFailHandler(Fail)
+  RunSpecs(t, "Oauth Suite")
+}
