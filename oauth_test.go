d4823fed01b5bd1523b905b85280514cea22817d
Meaglith Ma <genedna@gmail.com>
2014-05-11 16:19:45 +0800

    Add a ginkgo test case.

--- oauth_test.go
+++ oauth_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "bytes"
+  "encoding/json"
   "github.com/arkors/oauth/handler"
   "github.com/arkors/oauth/model"
   "github.com/go-martini/martini"
@@ -13,8 +14,9 @@ import (
   "net/http/httptest"
 )
 
-var _ = Describe("Todo", func() {
+var _ = Describe("Testing For Oauth App And Token", func() {
   It("POST '/v1/apps/:app' will returns a 201 status code", func() {
+
     m := martini.Classic()
     m.Use(render.Renderer())
     m.Use(Db())
@@ -22,9 +24,22 @@ var _ = Describe("Todo", func() {
       r.Post("/:app", binding.Json(model.Application{}), handler.RegistryApp)
     })
     response := httptest.NewRecorder()
-    request, _ := http.NewRequest("POST", "/v1/apps/232", bytes.NewReader([]byte("{\"sign\":\"5024442115e7bd738354c1fac662aed5\"}")))
+    request, _ := http.NewRequest("POST", "/v1/apps/233", bytes.NewReader([]byte("{\"sign\":\"5024442115e7bd738354c1fac662aed5\"}")))
     request.Header.Set("Content-Type", "application/json")
     m.ServeHTTP(response, request)
+
+    type Result struct {
+      App int
+      Key string
+    }
+
+    var result Result
+    err := json.Unmarshal(response.Body.Bytes(), &result)
+
+    Ω(err).Should(BeNil())
+    Ω(result.App).Should(BeEquivalentTo(233))
+    Ω(len(result.Key)).Should(BeNumerically("==", 32))
+
     Expect(response.Code).To(Equal(http.StatusCreated))
   })
 
